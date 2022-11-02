786b19289d56cc75e00fc49ecd6de8ae49f6ea53
WheatbixCodes <wheatbix89@gmail.com>
2022-11-02 08:36:44 +0200

    Fix

---
+++ ANDROID/Wil Project/app/src/main/java/com/varsitycollege/WIL/aboutEmployability.java
@@ -0,0 +1,14 @@
+package com.varsitycollege.WIL;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aboutEmployability extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_employability);
+    }
+}
