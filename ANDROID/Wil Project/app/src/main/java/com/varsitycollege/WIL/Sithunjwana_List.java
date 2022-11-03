93669dd9e6a9b6b5aed112a975fc128427f85374
WheatbixCodes <wheatbix89@gmail.com>
2022-11-03 10:28:49 +0200

    ListStart

---
+++ ANDROID/Wil Project/app/src/main/java/com/varsitycollege/WIL/Sithunjwana_List.java
@@ -0,0 +1,14 @@
+package com.varsitycollege.WIL;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sithunjwana_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sithunjwana_list);
+    }
+}
