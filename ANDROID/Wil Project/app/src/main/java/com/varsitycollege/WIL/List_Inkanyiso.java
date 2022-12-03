4127e9f51fb4a386a349ea2449e3f16342eacab4
brettpresscott <87628822+brettpresscott@users.noreply.github.com>
2022-12-03 21:20:37 +0200

    Update List_Inkanyiso.java

--- ANDROID/Wil Project/app/src/main/java/com/varsitycollege/WIL/List_Inkanyiso.java
+++ ANDROID/Wil Project/app/src/main/java/com/varsitycollege/WIL/List_Inkanyiso.java
@@ -1,5 +1,6 @@
 package com.varsitycollege.WIL;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,27 +10,52 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class List_Inkanyiso<listView, arrayAdapter> extends AppCompatActivity {
-    ListView listView;
+    private FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference myRef;
+    private ArrayList<String> categoryList;
+    private ListView listView;
+    private ArrayAdapter <String> categoryArrayAdapter;
+    public static String categoryName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_inkanyiso);
-        String[] studentList = {"studentA", "studentB", "studentC", "studentD"};
-        ArrayAdapter arrayAdapter;
-        listView = (ListView) findViewById(R.id.list);
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, studentList);
-        listView.setAdapter(arrayAdapter);
 
+        listView = findViewById(R.id.list);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        categoryList = new ArrayList<>();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        myRef = mFirebaseDatabase.getReference("ItemsList").child("Inkanyiso");
+        categoryArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categoryList);
+        listView.setAdapter(categoryArrayAdapter);
+
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(List_Inkanyiso.this, studentList[position].toString(),
-                        Toast.LENGTH_LONG).show();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    //snapshot.getChildren();
+                    categoryName = dataSnapshot.child("itemName").getValue().toString();
+
+                    categoryList.add(categoryName);
+                }
+                listView.setAdapter(categoryArrayAdapter);
             }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
     }
 }
